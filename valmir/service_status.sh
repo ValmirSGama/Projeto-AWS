835a2a9df3269893a9c92418a995d5876a43735c
Valmir Gama <111182775+ValmirSGama@users.noreply.github.com>
2024-04-13 21:27:37 -0300

    Update service_status.sh

--- valmir/service_status.sh
+++ valmir/service_status.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 service_name="Apache"
-timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 
-    if systemctl is-active httpd; then
-        echo "Data/Hora: $timestamp - $service_name - Serviço HTTPD - Status: O serviço está online" >> "/mnt/efs/valmir/httpd-online.txt"
-    else
-        echo "Data/Hora: $timestamp - $service_name - Serviço HTTPD - Status: O serviço está offline" >> "/mnt/efs/valmir/httpd-offline.txt"
-    fi
+# Captura o timestamp atual e ajusta para UTC-3 (subtraindo 3 horas)
+timestamp=$(date -d "-3 hours" +"%Y-%m-%d %H:%M:%S")
+
+if systemctl is-active httpd; then
+    echo "Data/Hora: $timestamp - $service_name - Serviço HTTPD - Status: O serviço está online" >> "/mnt/nfs/valmir/httpd-online.txt"
+else
+    echo "Data/Hora: $timestamp - $service_name - Serviço HTTPD - Status: O serviço está offline" >> "/mnt/nfs/valmir/httpd-offline.txt"
+fi
 
